11e358345a5a7fc397c7f5ee39023c712bf4b5d1
Daniel Martins <danielsantosmartins.dev@gmail.com>
2024-06-24 23:21:20 -0300

    criadas novas rotas, base e partials

--- chacaramartins/urls.py
+++ chacaramartins/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from chacaramartins.views import index
+from chacaramartins.views import index, galeria, localizacao
 
 urlpatterns = [
-    path('', index)
+    path('', index, name='inicio'),
+    path('galeria/', galeria, name='galeria'),
+    path('localizacao/', localizacao, name='localizacao')
 ]
--- chacaramartins/views.py
+++ chacaramartins/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'inicio/index.html')
+    return render(request, 'chacaramartins/index.html')
+
+def galeria(request):
+    return render(request, 'chacaramartins/galeria.html')
+
+def localizacao(request):
+    return render(request, 'chacaramartins/localizacao.html')
